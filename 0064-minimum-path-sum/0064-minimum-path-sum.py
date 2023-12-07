331f35120473f941c5c04f2e2279dba7e3bd96ba
Raphael Alves <131635716+AugustRaph@users.noreply.github.com>
2023-12-07 12:54:42 -0300

    Time: 55 ms (96.19%), Space: 14.5 MB (76.91%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m = len(grid)
+        n = len(grid[0])
+        
+        # Traverse the first row and first column and calculate the sum 
+        # of each cell by adding the value of the previous cell in the same row or column
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j-1]
+        
+        # Traverse the remaining cells and calculate the minimum sum to reach each cell
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        # Return the minimum sum to reach the last cell
+        return grid[m-1][n-1]
